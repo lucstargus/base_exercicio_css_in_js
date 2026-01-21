590e479f9c03eba68155f049e7dc153b6934afd3
Lucas <158581359+lucstargus@users.noreply.github.com>
2026-01-21 17:54:22 -0300

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,12 @@
-import styles from './Hero.module.css'
+import { HeroContainer, Titulo, Subtitulo } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <HeroContainer>
+    <Titulo>As melhores vagas para tecnologia</Titulo>
+    <Subtitulo>
+      Encontre oportunidades que combinam com você
+    </Subtitulo>
+  </HeroContainer>
 )
 
 export default Hero
