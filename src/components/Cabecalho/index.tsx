4e2d0b236e648578dd53a6b4e76238ac816fd04c
Lucas <158581359+lucstargus@users.noreply.github.com>
2026-01-21 17:49:14 -0300

    Update index.tsx

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { Header, Titulo } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+  <Header>
+    <Titulo>EBAC Jobs</Titulo>
+  </Header>
 )
 
 export default Cabecalho
